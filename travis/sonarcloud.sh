7a0fbc4fa24c3fb473b6afce5e5c92536f4ea4c7
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-06-03 11:27:32 +0200

    fix travis

---
+++ travis/sonarcloud.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    mvn sonar:sonar-Pcoverage \
+    -Dsonar.host.url=https://sonarcloud.io
+    -Dsonar.pullrequest.base=master \
+    -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST} \
+    -Dsonar.pullrequest.branch=${TRAVIS_PULL_REQUEST_BRANCH} \
+    -Dsonar.pullrequest.provider=GitHub \
+    -Dsonar.pullrequest.github.repository=ubl-analytics/analytics
+fi
+
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    mvn sonar:sonar -Pcoverage
+fi
