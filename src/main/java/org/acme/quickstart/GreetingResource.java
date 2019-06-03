f5103b92e7619d6225c3743ab429e5ef84b52e07
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-06-03 15:51:36 +0200

    remove codecov

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        System.out.println("hello");
+        System.out.println("hello fork3");
         return "hello";
     }
 }
