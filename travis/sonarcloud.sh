d1446d8698b210a029f6159a523bcadd6a582dd5
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-06-04 13:30:14 +0200

    change travis

--- travis/sonarcloud.sh
+++ travis/sonarcloud.sh
@@ -1,27 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    mvn sonar:sonar
+fi
+
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-    mvn sonar:sonar -Pcoverage \
+    mvn sonar:sonar \
     -Dsonar.pullrequest.base=master \
     -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST} \
     -Dsonar.pullrequest.branch=${TRAVIS_PULL_REQUEST_BRANCH} \
-    -Dsonar.pullrequest.provider=GitHub \
-    -Dsonar.pullrequest.github.repository=ubl-analytics/analytics  \
-    -Dsonar.projectKey=ubl-analytics_analytics  \
-    -Dsonar.projectName='UBL Analytics' \
-    -Dsonar.links.homepage='https://github.com/ubl-analytics/analytics' \
-    -Dsonar.links.ci='https://travis-ci.org/ubl-analytics/analytics' \
-    -Dsonar.links.scm='https://github.com/ubl-analytics/analytics' \
-    -Dsonar.links.issue='https://github.com/ubl-analytics/analytics/issues' \
-    -Dsonar.host.url='https://sonarcloud.io' \
-    -Dsonar.organization=ubl-analytics \
-    -Dsonar.sources=src \
-    -Dsonar.java.source='1.8' \
-    -Dsonar.java.binaries='.' \
-    -Dsonar.exclusions='**/*Test.java,**/*IT.java' \
-    -Dsonar.test.exclusions='**/*Test.java,**/*IT.java'
-fi
-
-if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-    mvn sonar:sonar -Pcoverage
+    -Dsonar.pullrequest.provider=GitHub
 fi
